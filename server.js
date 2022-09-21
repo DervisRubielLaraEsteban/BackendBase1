c8dde900053783d5b9f2c1f40092f6d52e9320e3
DervisRubielLaraEsteban <l19350298@tuxtepec.tecnm.mx>
2022-09-21 10:30:10 -0500

    Primer Commit

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 const express = require('express') 
+const messagesRouter = require('./routes/messages')
 
 class Server{
     constructor(){
       this.app = express()
+      this.paths = {
+         messages:"/api/v1/messages"
+
+        }
       this.routes()
+
     }
 
 routes(){
-    this.app.get('/', (req, res) => {
-        res.send('Hello World')
-    })
+    
+  //this.app.get('/', (req, res) => {
+    //res.send('Hello World')
+    //  }
+    this.app.use(this.paths.messages, messagesRouter)
 }
 
 listen(){
     this.app.listen(process.env.PORT,() => { //Poner un this aqui xd//
-        console.log("Backend en ejecución en el puerto", process.env.PORT)
+    console.log("Backend en ejecución en el puerto", process.env.PORT)
 })
 }
 }
