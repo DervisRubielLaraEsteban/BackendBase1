f90d82baf2b50913d703520843b0a74431c96763
DervisRubielLaraEsteban <l19350298@tuxtepec.tecnm.mx>
2022-09-28 22:05:13 -0500

    Parametros insertados en la consulta

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,7 +1,20 @@
 const { request, response } = require("express")
 
 const rootMessage = (req=request, res=response) => {
-    res.status(404).json({msg:'Mensajes'})
+    const {texto1, texto2}=req.query
+ //   console.log(req.query)/*El query tiene almacenado el mensaje Hola*/
+ //   if(!texto1 || !texto2){
+ //       res.status(400).json({
+ //        msg:'No se ha enviado los parametros necesarios, este EndPoint ocupa los parametros texto1 y texto2 '
+ //       })
+ //   }
+ if (!texto1){
+    res.status(400).json({msg:"Falta el parametro 'texto1'"})
+ }
+ if(!texto2){
+    res.status(400).json({msg:"Falta el parametro 'texto2'"})
+ }
+    res.status(200).json({msg: texto1 + ' ' + texto2})
 }
 /*PROBAR UN STATUS EN CADA UNA DE ELLAS: res.status(número del error).json*/
 const hiMessage =(req=request, res=response) => {
