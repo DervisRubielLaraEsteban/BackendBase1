5503362ffac599504e94b3aac87a6528566aaef5
DervisRubielLaraEsteban <l19350298@tuxtepec.tecnm.mx>
2022-10-26 10:17:47 -0500

    1ra consulta de bases a End Point

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,8 +1,165 @@
 const { request, response } = require("express");
+const { query } = require("../db/connection");
+const pool = require("../db/connection")
 
-const getUsers = (req = request, res = response) =>{
-console.log("Función getUsers")
-res.json({msg: "Función getUsers"})
+const getUsers = async(req = request, res = response) =>{
+    let conn;
+    
+try {
+
+    conn= await pool.getConnection()
+    const users =  await conn.query("SELECT * FROM usuarios", (error) => {throw new Error(error)})
+
+    if(!users){
+        res.status(404).json({msg:"No se encontrarón registros"})
+        return
+    }
+    res.json({users})
+} catch (error){
+    console.log(error)
+    res.status(500).json({error})
+}finally {
+    if(conn){
+        conn.end()
+    }
+}
 }
 
-module.exports = {getUsers}
+
+const getUserbyID = async(req = request, res = response) =>{
+    const {id} = req.params
+    let conn;
+//console.log("Función getUsers")
+//res.json({msg: "Función getUsers"})
+
+//CONTROL DE EXCEPSIONES//
+
+try {
+
+    conn= await pool.getConnection()
+
+    const [user] = await conn.query(`SELECT * FROM usuarios WHERE ID = ${id}`, (error) => {throw new Error(error)})
+
+    if(!user){
+        res.status(404).json({msg:`No se encontraró registro con el ID ${id}`})
+        return
+    }
+    res.json({user})
+} catch (error){
+    console.log(error)
+    res.status(500).json({error})
+} finally {
+    if(conn){
+        conn.end()
+    }
+}
+}
+
+/////
+
+const userDeletedByID = async(req = request, res = response) =>{
+    const {id} = req.query
+    let conn;
+
+try {
+
+    conn= await pool.getConnection()
+
+    const {affectedRows} = await conn.query(`UPDATE Usuarios SET Activo = 'N' WHERE ID = ${id}`, (error) => {throw new Error(error)})
+
+    if(affectedRows === 0){
+        res.status(404).json({msg:`No se pudo eliminar el registro con el ID ${id}`})
+        return
+    }
+       res.json({msg:`El usuario con el ${id} se eliminó satisfactoriamente`})
+
+      }     
+      
+      catch (error){
+    console.log(error)
+    res.status(500).json({error})
+} finally {
+    if(conn){
+        conn.end()
+    }
+}
+}
+//1ra consulta de bases a end point
+
+///
+
+const addUser = async(req = request, res = response) =>{
+    const{
+        Usuarios,
+        Nombre,
+        Apellidos,
+        Edad,
+        Genero,
+        Contrasena,
+        Fecha_Nacimiento,
+        Activo
+    }= req.body
+    
+    if(
+        !Usuarios || 
+        !Nombre ||
+        !Apellidos ||
+        !Edad ||
+        !Contrasena ||
+        !Activo
+    ){
+        res.status(400).json({msg:"Falta información del usuario"})
+        return
+    }
+
+    let conn;
+
+try {
+
+    conn= await pool.getConnection()
+
+
+
+
+    const {affectedRows} = await conn.query(`
+    
+    INSERT INTO Usuarios (
+        Usuarios,
+        Nombre,
+        Apellidos,
+        Edad,
+        Genero,
+        Contrasena,
+        Fecha_Nacimiento,
+        Activo
+    ) VALUES (
+        '${Usuarios}',
+        '${Nombre}',
+        '${Apellidos}',
+        ${Edad},
+        '${Genero}',
+        '${Contrasena}',
+        '${Fecha_Nacimiento}',
+        '${Activo}'
+    )
+    
+    `, (error) => {throw new Error(error)})
+
+    if(affectedRows === 0){
+        res.status(404).json({msg:`No se pudo agregar el registro del usuario ${Usuarios}`})
+        return
+    }
+       res.json({msg:`El usuario ${Usuarios} se agregó satisfactoriamente`})
+
+      }     
+      
+      catch (error){
+    console.log(error)
+    res.status(500).json({error})
+} finally {
+    if(conn){
+        conn.end()
+    }
+}
+}
+module.exports = {getUsers, getUserbyID, userDeletedByID, addUser}
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,9 +1,20 @@
 const {Router} = require("express")
-const {getUsers} = require("../controllers/usuarios")
+const {getUsers, getUserbyID, userDeletedByID, addUser} = require("../controllers/usuarios")
 const router = Router()
 
 //AQUI SE DEFINE LA RUTA DE http://localhost:4000/api/v1/usuarios//
 
+//GET//
+
 router.get("/", getUsers)
+router.get("/id/:id",getUserbyID)
+
+//DELETE//
+
+router.delete("/",userDeletedByID)
+
+//POST//
+
+router.post("/",addUser)
 
 module.exports = router
